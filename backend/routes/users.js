db6b92583fcf453c4877041352ae0bd3f665779f
alexandre-poirier <alex481poirier@hotmail.com>
2020-07-15 16:35:40 -0400

    Started implementing the login endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require("crypto");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* Create user JWT. */
+router.post('/user/login', function(req, res, next) {
+  let userName = req.body['user'];
+  let password = req.body['password'];
+  let isAdmin = req.body['isAdmin'];
+
+
+
   res.send('respond with a resource');
 });
 
