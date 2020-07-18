6d8786af9a6b227acb2d813d4dd7c7ba363d4c81
alexandre-poirier <alex481poirier@hotmail.com>
2020-07-18 16:11:27 -0400

    Update data-seed.js
    Formatting

--- backend/prisma/data-seed.js
+++ backend/prisma/data-seed.js
@@ -4,19 +4,19 @@ const prisma = new PrismaClient();
 async function seed() {
   let newBuilding1 = await prisma.building.create({
     data: {
-      address: "123 special street"
-    }
+      address: "123 special street",
+    },
   });
-  
+
   let newBuilding2 = await prisma.building.create({
     data: {
-      address: "456 special street"
-    }
+      address: "456 special street",
+    },
   });
 }
 
 seed()
-  .catch(e => console.error(e))
+  .catch((e) => console.error(e))
   .finally(async () => {
     await prisma.disconnect();
   });
